c96fa1da40d92a3820baefce3bdb5c99aad6887b
juhyeok <loyjs@naver.com>
2017-10-25 15:55:42 +0900

    analysis finished: run.sh

--- src/shellScript/run.sh
+++ src/shellScript/run.sh
@@ -8,26 +8,26 @@ set -m
 
 echo "analysis start..."
 
-# foldername에 있는 모든 file을 실행한다.
-# analysis.sh는 어디에?
+# checked: foldername에 있는 모든 file을 실행한다.
+# checked: analysis.sh는 어디에?
 for LINE in `ls $foldername`
 do
         /bin/bash analysis.sh $foldername'/'$LINE &
 done
 
-# wait all background process.
+# checked: wait all background process.
 wait
 #while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
 
-# what is metricData?
+# checked: what is metricData?
 rm metricData.*
 echo "analysis complete"
 
 echo "codeCopy check start..."
 
-# why move moss.pl to foldername?
-# what is moss.pl?
-# why re move moss.pl to original dir?
+# checked: why move moss.pl to foldername?
+# checked: what is moss.pl?
+# checked: why re move moss.pl to original dir?
 mv moss.pl $foldername
 cd $foldername
 ./moss.pl -l python *.py
